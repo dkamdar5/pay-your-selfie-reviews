623c61eb112b742a407a8a22d969d751fd10abd2
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2016-05-30 00:50:24 -0500

    Fixed iOS review implementation

--- reviews.js
+++ reviews.js
@@ -8,21 +8,20 @@ $(document).ready(function() {
 
     // get individual iOS user reviews
     $.get("https://itunes.apple.com/rss/customerreviews/id=938806572/json", function(data){
-	// ignore first, stop at 2 because we only need to retrieve the latest review.
-	for (i=1; i < 2; i++) {
-	    // create variables for the values retrieved
-	    var entry = data.feed.entry[i];
-	    var author = entry.author.name.label;
-	    var review = entry.content.label;
-	    var rating = entry["im:rating"].label;
-	    var title = entry.title.label;
+	// ignore first, 2nd element is the latest review.
+	var entry = data.feed.entry[1];
+	
+	// create variables for the values retrieved
+	var author = entry.author.name.label;
+	var review = entry.content.label;
+	var rating = entry["im:rating"].label;
+	var title = entry.title.label;
 
-	    // assign values
-	    $("#ios-author").html(author);
-	    $("#ios-rating").html(rating);
-	    $("#ios-review-full").html(review);
-	    $("#ios-review-title").html(title);
-	}
+	// assign values
+	$("#ios-author").html(author);
+	$("#ios-rating").html(rating);
+	$("#ios-review-full").html(review);
+	$("#ios-review-title").html(title);
     }, "json");  // specify format
 
     // get Android user review
